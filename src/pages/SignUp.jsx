5ed75088021f7c3216f0fc5dcc04d3588bedbaba
Ojima <akbenngold@gmail.com>
2024-05-05 15:29:10 +0100

    screen orirn

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,42 @@
 import React from "react";
-
+import InputComponent from "../components/InputComponent";
+import Button from "../utilities/Button";
+import { IoMdRadioButtonOn, IoMdRadioButtonOff } from "react-icons/io";
+import { Link } from "react-router-dom";
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className="signin">
+      <h2>
+        Become a
+        <br /> Big Spender!
+      </h2>
+      <form action="">
+        <div className="form">
+          <InputComponent label="Full Name" user={true} />
+          <InputComponent label="Phone Number" phone={true} />
+          <InputComponent label="E-mail" message={true} />
+          <InputComponent label="4-Digit Passcode" eye={true} padlock={true} />
+          <InputComponent label="Confirm Passcode" eye={true} padlock={true} />
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Button label="SIGN UP" />
+        </div>
+        <p style={{ textAlign: "center" }}>
+          Already have an account?{" "}
+          <Link to="/signin">
+            <span>Sign in </span>
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
